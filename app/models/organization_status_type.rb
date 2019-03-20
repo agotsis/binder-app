e6ac808e071a62940a1d2b041920f9a341fb11e8
Lorraine Zhang <lorrainz@andrew.cmu.edu>
2019-03-20 03:43:31 +0000

    added scopes for build, electrical, and general statuses

--- app/models/organization_status_type.rb
+++ app/models/organization_status_type.rb
@@ -17,4 +17,9 @@ class OrganizationStatusType < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => true
 
   has_many :organization_statuses, dependent: :destroy
+  
+  scope :build, ->  { where(is_build: true) }
+  scope :electrical, -> {where(is_electrical: true) }
+  scope :general, -> { where("is_build IS FALSE AND is_electrical IS FALSE") }
+  
 end
