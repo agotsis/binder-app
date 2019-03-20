7b90eece39fa9df01349105a74b93289ec2c56cb
Lorraine Zhang <lorrainz@andrew.cmu.edu>
2019-03-20 03:23:55 +0000

    updated seed files and seeds.rb for new schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -58,7 +58,7 @@ puts '  Organization Status Types'
 csv_text = File.read(Rails.root.join('lib', 'seeds', gdrive_doc + 'organization_status_types.csv'))
 csv = CSV.parse(csv_text, :headers => true)
 csv.each do |row|
-  OrganizationStatusType.create(name: row['name'].strip, display: row['display'] == "TRUE")
+  OrganizationStatusType.create(name: row['name'].strip, display: row['display'] == "TRUE", is_build: row['is_build'] == "FALSE", is_electrical: row['is_electrical'] == "FALSE")
 end
 
 puts '  Charge Types'
